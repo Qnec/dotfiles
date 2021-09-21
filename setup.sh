a47f10f28c7afb250bad1955632dcfc61913fbbf
Qnec <81662803+Qnec@users.noreply.github.com>
2021-09-21 12:05:08 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -31,3 +31,4 @@ git clone https://github.com/Qnec/dotfiles.git
 
 cp -R ./dotfiles/.config ~/
 cp ./dotfiles/.bash_aliases ~/
+sudo cp ./dotfiles/configurationFiles/lightdm.conf /etc/lightdm/lightdm.conf
