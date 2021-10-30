c9d3c62b75443251190d1138a3f2e63af1e67e9e
Qnec <Qnec@github.com>
2021-10-30 12:40:14 -0700

    xfce terminal config

--- setupScripts/defaultConfigurationFilesInstall.sh
+++ setupScripts/defaultConfigurationFilesInstall.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cp -R ./dotfiles/.config/mc ~/.config/mc
-cp ./dotfiles/configurationFiles/.bash_aliases ~/
-cp ./dotfiles/configurationFiles/.autostart.sh ~/
+cp -R ./dotfiles/.config/xfce4/terminal ~/.config/xfce4/terminal
+cp -R ./dotfiles/configurationFiles/.bash_aliases ~/
+cp -R ./dotfiles/configurationFiles/.autostart.sh ~/
 sudo cp ./dotfiles/configurationFiles/lightdm.conf /etc/lightdm/lightdm.conf
 #cp -R ./dotfiles/.config ~/
