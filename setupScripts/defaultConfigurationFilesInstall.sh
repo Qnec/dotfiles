19fc26021d068ecbdb20debd357644369e38ea6a
Qnec <Qnec@github.com>
2021-10-30 12:49:26 -0700

    oops

--- setupScripts/defaultConfigurationFilesInstall.sh
+++ setupScripts/defaultConfigurationFilesInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp -R ./dotfiles/.config/mc ~/.config/mc
-cp -R ./dotfiles/.config/xfce4/terminal ~/.config/xfce4/terminal
+cp -R --parents ./dotfiles/.config/xfce4/terminal ~/.config/xfce4/terminal
 cp -R ./dotfiles/configurationFiles/.bash_aliases ~/
 cp -R ./dotfiles/configurationFiles/.autostart.sh ~/
 sudo cp ./dotfiles/configurationFiles/lightdm.conf /etc/lightdm/lightdm.conf
